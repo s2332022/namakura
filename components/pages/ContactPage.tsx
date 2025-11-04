fac9543bad3033bdf7cad594be5bb2fd8ca30360
y o <funifura559@gmail.com>
2025-11-04 15:06:08 +0900

    Contact: add extra bottom spacing (pb-24)

--- components/pages/ContactPage.tsx
+++ components/pages/ContactPage.tsx
@@ -4,7 +4,7 @@ import PageContainer from './PageContainer';
 const ContactPage: React.FC = () => {
   return (
     <PageContainer title="Contact">
-      <div className="text-center space-y-4">
+      <div className="text-center space-y-4 pb-24">
         <p>ライブ出演のご依頼、その他各種お問い合わせは、下記のメールアドレスまでご連絡ください。</p>
         <a href="mailto:contact@namakurametro.com" className="text-2xl text-yellow-400 hover:text-yellow-300 transition-colors">
           メアドなかったわ@gmail.com
